c4e3a1aaaa80e230c11275204ff1255e7c373d3f
Arindam Chowdhury <arindam.nitdgp14@gmail.com>
2025-05-14 15:27:42 -0400

    modify niaid download

--- examples/niaid/download_dataset.py
+++ examples/niaid/download_dataset.py
@@ -1,7 +1,10 @@
-import os
+import os, pdb
 import tarfile
 import requests
+import pandas as pd
 from tqdm import tqdm
+from pathlib import Path
+from Bio.PDB.MMCIF2Dict import MMCIF2Dict
 
 def download_with_progress(url, download_path):
     response = requests.get(url, stream=True)
@@ -33,10 +36,33 @@ def download_and_extract_tar_gz(url, download_path, extract_to):
     os.remove(download_path)
     print(f"Removed archive: {download_path}")
 
+def preprocess(dir_path, save_path):
+    list_of_cif = []
+    print(f"\nPreprocessing ...")
+    for file in tqdm(Path(dir_path).glob('*.cif'), total=242517):
+         dico = MMCIF2Dict(file)
+         temp = pd.DataFrame.from_dict(dico, orient='index')
+         temp = temp.transpose()
+         temp.insert(0, 'Filename', Path(file).stem)
+         list_of_cif.append(temp)
+    df = pd.concat(list_of_cif)
+    df = df[['Filename','_cell_length_a', '_cell_length_b', 
+                '_cell_length_c', '_cell_angle_alpha', '_cell_angle_beta',
+                '_cell_angle_gamma', '_cell_volume', '_atom_site_label',
+                '_atom_site_type_symbol', '_atom_site_fract_x', '_atom_site_fract_y', 
+                '_atom_site_fract_z', '_atom_type_partial_charge']]
+    
+    df.to_parquet(save_path+'/niaid.parquet.gzip',compression='gzip')
+    print(f"\nSaved data to disk ...")
+
+
 url = "https://zenodo.org/records/13891643/files/ARCMOF_20241004.tar.gz?download=1"
 download_path = "./dataset/raw/ARCMOF_20241004.tar.gz"       
-extract_to = "./dataset/raw"                     
+extract_to = "./dataset/raw"
+dir_path = extract_to+'/ARCMOF_20241004/'                     
 
-download_and_extract_tar_gz(url, download_path, extract_to)
+if not os.path.isdir(dir_path):
+    download_and_extract_tar_gz(url, download_path, extract_to)
+preprocess(dir_path, extract_to)
 
 
